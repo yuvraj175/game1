b07ed2145ad86933659bcd2ddf28e40fb31e7d03
yuvraj175 <77335556+yuvraj175@users.noreply.github.com>
2021-04-03 10:06:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var garden, gardenImg;
+
+
+
+
+
+function preload() {
+    //load the images here
+    gardenImg = loadImage("garden.png");
+
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    
+
+
+}
+
+function draw() {
+
+    background("gardenImg");
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
